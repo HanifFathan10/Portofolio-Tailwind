a7414fcd77ebab27dba4b77ca26fe34f1bede2a6
Haniep Fathan Riziq <haniffathan33@gmail.com>
2023-10-09 01:01:14 +0700

    solve problem

--- dist/js/script.js
+++ dist/js/script.js
@@ -19,13 +19,13 @@ window.onscroll = function () {
 const hamburger = document.querySelector("#hamburger");
 const navMenu = document.querySelector("#nav-menu");
 
-hamburger.addEventListener("click", () => {
+hamburger.addEventListener("click", ()=> {
   hamburger.classList.toggle("hamburger-active");
   navMenu.classList.toggle("hidden");
 });
 
 // klik diluar hamburger
-window.addEventListener("click", (e) => {
+window.addEventListener("click", (e)=> {
   if (e.target != hamburger && e.target != navMenu) {
     hamburger.classList.remove("hamburger-active");
     navMenu.classList.add("hidden");
@@ -35,30 +35,29 @@ window.addEventListener("click", (e) => {
 // dark mode
 const darkToggle = document.querySelector("#dark-toggle");
 const html = document.querySelector("html");
-const dark = document.getElementById("imgDark");
-const darkModeMediaQuery = window.matchMedia("(prefers-color-scheme: dark)").matches;
 
-darkToggle.addEventListener("click", () => {
-  if (darkToggle.checked && darkModeMediaQuery) {
+darkToggle.addEventListener("click", ()=> {
+  if (darkToggle.checked) {
     html.classList.add("dark");
     localStorage.theme = "dark";
-    document.getElementById("gambarA").style.display = "none";
     document.getElementById("gambarB").style.display = "block";
+    document.getElementById("gambarA").style.display = "none";
   } else {
     html.classList.remove("dark");
     localStorage.theme = "light";
     document.getElementById("gambarB").style.display = "none";
-    document.getElementById("gambarA").style.display = "block";
+  document.getElementById("gambarA").style.display = "block";
   }
 });
 
-// pindahkan posisi toggle sesuai mode menggunakan ternary operator
-{
-  localStorage.theme === "dark" ? (darkToggle.checked = true) : (darkToggle.checked = false);
+// pindahkan posisi toggle sesuai mode
+if (localStorage.theme === "dark" || (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
+  darkToggle.checked = true;
+} else {
+  darkToggle.checked = false;
 }
 
-// agar gambar hanya 1 ketika function toggle belum dijalankan
-if (localStorage.theme === "light") {
+if (localStorage.theme === "light" || (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
   document.getElementById("gambarB").style.display = "none";
   document.getElementById("gambarA").style.display = "block";
 } else {
@@ -67,21 +66,21 @@ if (localStorage.theme === "light") {
 }
 
 // validasi form
-const contactForm = document.getElementById("contact-form");
+const contactForm = document.getElementById('contact-form');
 
-contactForm.addEventListener("submit", (e) => {
+contactForm.addEventListener('submit', e => {
   e.preventDefault();
-
+  
   const url = e.target.action;
   const formData = new FormData(contactForm);
 
   fetch(url, {
-    method: "post",
+    method: 'post',
     body: formData,
-    mode: "no-cors",
+    mode: 'no-cors',
   })
-    .then(() => {
-      window.location.href = "thankyou.html";
-    })
-    .catch((e) => alert("error brow"));
+  .then(() => {
+    window.location.href = 'thankyou.html';
+  }).catch((e) => alert('error brow'))
+
 });
