5e314e84ec5ae14f32f5677cc311dea0e1d90bba
Haniep Fathan Riziq <haniffathan33@gmail.com>
2023-10-09 00:44:28 +0700

    Merubah UI dan mengganti text ke bahasa inggris

--- dist/js/script.js
+++ dist/js/script.js
@@ -19,13 +19,13 @@ window.onscroll = function () {
 const hamburger = document.querySelector("#hamburger");
 const navMenu = document.querySelector("#nav-menu");
 
-hamburger.addEventListener("click", function () {
+hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("hamburger-active");
   navMenu.classList.toggle("hidden");
 });
 
 // klik diluar hamburger
-window.addEventListener("click", function (e) {
+window.addEventListener("click", (e) => {
   if (e.target != hamburger && e.target != navMenu) {
     hamburger.classList.remove("hamburger-active");
     navMenu.classList.add("hidden");
@@ -35,40 +35,53 @@ window.addEventListener("click", function (e) {
 // dark mode
 const darkToggle = document.querySelector("#dark-toggle");
 const html = document.querySelector("html");
+const dark = document.getElementById("imgDark");
+const darkModeMediaQuery = window.matchMedia("(prefers-color-scheme: dark)").matches;
 
-darkToggle.addEventListener("click", function () {
-  if (darkToggle.checked) {
+darkToggle.addEventListener("click", () => {
+  if (darkToggle.checked && darkModeMediaQuery) {
     html.classList.add("dark");
     localStorage.theme = "dark";
+    document.getElementById("gambarA").style.display = "none";
+    document.getElementById("gambarB").style.display = "block";
   } else {
     html.classList.remove("dark");
     localStorage.theme = "light";
+    document.getElementById("gambarB").style.display = "none";
+    document.getElementById("gambarA").style.display = "block";
   }
 });
 
-// pindahkan posisi toggle sesuai mode
-if (localStorage.theme === "dark" || (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
-  darkToggle.checked = true;
+// pindahkan posisi toggle sesuai mode menggunakan ternary operator
+{
+  localStorage.theme === "dark" ? (darkToggle.checked = true) : (darkToggle.checked = false);
+}
+
+// agar gambar hanya 1 ketika function toggle belum dijalankan
+if (localStorage.theme === "light") {
+  document.getElementById("gambarB").style.display = "none";
+  document.getElementById("gambarA").style.display = "block";
 } else {
-  darkToggle.checked = false;
+  document.getElementById("gambarB").style.display = "block";
+  document.getElementById("gambarA").style.display = "none";
 }
 
 // validasi form
-const contactForm = document.getElementById('contact-form');
+const contactForm = document.getElementById("contact-form");
 
-contactForm.addEventListener('submit', e => {
+contactForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  
+
   const url = e.target.action;
   const formData = new FormData(contactForm);
 
   fetch(url, {
-    method: 'post',
+    method: "post",
     body: formData,
-    mode: 'no-cors',
+    mode: "no-cors",
   })
-  .then(() => {
-    window.location.href = 'thankyou.html';
-  }).catch((e) => alert('error brow'))
-
+    .then(() => {
+      window.location.href = "thankyou.html";
+    })
+    .catch((e) => alert("error brow"));
 });
