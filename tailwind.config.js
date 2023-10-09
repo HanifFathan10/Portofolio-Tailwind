f509ada2b2fff620b7240fa7bd9b97bf30a87d8f
Haniep Fathan Riziq <haniffathan33@gmail.com>
2023-10-09 12:19:52 +0700

    Revert "finnal project"
    This reverts commit 34ba68787e954e44d23fff8a08c2165e56016cc0.

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,7 @@ module.exports = {
         primary: '#155e75',
         secondary: '#64748b',
         dark: '#171717',
-        deep: '#121212',
+        deep: '#121212'
       },
       screens: {
         '2xl': '1320px',
